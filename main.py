36160fde1dd0172006377aeb795d62bc74c83d61
anaplopes <anapalopes0103@gmail.com>
2023-03-31 16:38:03 -0300

    finalizado com todas regras aplicadas

--- main.py
+++ main.py
@@ -1,67 +1,46 @@
 import sys
-from enum import Enum
+import json
 from typing import List
-from dataclasses import dataclass
 
 
-class Operation(str, Enum):
-    BUY = "buy"
-    SELL = "sell"
+def tax_percentage(operations: List[dict]) -> List[dict]:
+    qtd_shares = 0
+    current_weighted_average = 0
+    amount_op_result = 0
 
-
-@dataclass
-class In:
-    operation: Operation
-    unit_cost: float
-    quantity: int
-
-
-@dataclass
-class Out:
-    tax: float
-
-
-def tax_percentage(operations: List[In]) -> List[Out]:
-    """Função responsável por calcular o percentual de imposto pago
-    sobre o lucro obtido em operações de venda.
-
-    Args:
-        operations (List[In]): operações de compra e venda
-
-    Returns:
-        List[Out]: percentual de imposto pago nas operações
-    """
-    out = []
-    prejuizo = 0
     for op in operations:
-        if op.operation == "buy":
-            out.append({"tax":0.00})
-        else:
-            price_sell = op.unit_cost * op.quantity
-            if price_sell <= 20000:
-                out.append({"tax":0.00})
+
+        if op["operation"] == "buy":
+            op.update({"tax": 0.00})
+            stock = qtd_shares * current_weighted_average
+            price_purchase = op["quantity"] * op["unit-cost"]
+            current_weighted_average = (stock + price_purchase) / (qtd_shares + op["quantity"])
+            qtd_shares += op["quantity"]
+
+        if op["operation"] == "sell":
+            op_result = op["unit-cost"] - current_weighted_average
+            price_sell = op["unit-cost"] * op["quantity"]
+            if op_result > 0 and price_sell <= 20000:
+                op.update({"tax": 0.00})
+                qtd_shares -= op["quantity"]
+
             else:
-                ...
+                amount_op_result += (op_result * op["quantity"])
+                tax = round(amount_op_result * (20 / 100), 2) if amount_op_result > 0 else 0.00
+                op.update({"tax": tax})
+                qtd_shares -= op["quantity"]
+                amount_op_result = 0 if amount_op_result > 0 else amount_op_result
 
-    return out
-
-
-
-
-# ENTRADA
-# [{"operation":"buy", "unit_cost":10.00, "quantity": 10000}, {"operation":"sell", "unit_cost":20.00, "quantity": 5000}]
-# [{"operation":"buy", "unit_cost":20.00, "quantity": 10000}, {"operation":"sell", "unit_cost":10.00, "quantity": 5000}]
-
-
-# SAIDA
-# [{"tax":0.00}, {"tax":10000.00}]
-# [{"tax":0.00}, {"tax":0.00}]
-
+    return operations
 
 
 for line in sys.stdin:
     if "" == line.rstrip():
         break
 
-    result = tax_percentage(operations=line)
-    sys.stdout.write(result)
+    operations = json.loads(line)
+    tax = tax_percentage(operations=operations)
+    print(json.dumps(tax) + '\n')
+    # sys.stdout.write(json.dumps(tax) + '\n\n')
+    # sys.stdout.flush()
+sys.exit('Finished...')
